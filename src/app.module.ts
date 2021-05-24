074e333a11d6b2c6e7a011a251c72d05830c07b1
peter <peter@capa.ai>
2021-05-24 12:25:59 +0900

    env seperated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,39 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { env } from './env'
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { BoardsModule } from './boards/boards.module';
 import { Users } from './users/users.entity';
 import { Comments } from './comments/comments.entity';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 import { Boards } from './boards/boards.entity';
+import * as Joi from 'joi'
 
 @Module({
   imports: [
+    // ENV config
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.local',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'local').required(),
+        HOST: Joi.string().required(),
+        PORT: Joi.string().required(),
+        USERNAME: Joi.string().required(),
+        PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    // DB config
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: env.HOST,
-      port: env.PORT,
-      username: env.USERNAME,
-      password: env.PASSWORD,
-      database: env.DB_NAME,
+      host: process.env.HOST,
+      port: Number(process.env.PORT),
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DB_NAME,
       entities: [
         Users,
         Comments,
@@ -27,6 +43,7 @@ import { Boards } from './boards/boards.entity';
       ],
       namingStrategy: new SnakeNamingStrategy()
     }),
+    // GraphQL module config
     GraphQLModule.forRoot({
       autoSchemaFile: true,
       debug: true,
